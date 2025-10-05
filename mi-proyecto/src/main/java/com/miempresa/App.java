973866328001576d32a32a0e06df2018efa811a4
Dayanna <dayannazevallosvaldez@gmail.com>
2025-10-05 12:33:10 -0500

    Mi primer proyecto con maven y jenkins

---
+++ mi-proyecto/src/main/java/com/miempresa/App.java
@@ -0,0 +1,16 @@
+package com.miempresa;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println("Hola Java");
+        }
+
+
+        public int sumar (int a, int b){
+
+            return a+b;
+        }
+}
